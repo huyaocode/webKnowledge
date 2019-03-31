e6e143f4c04726ad421c3842c7ac97bd230f5bbf
huyaocode <2385249676@qq.com>
2019-03-31 20:50:06 +0800

    防抖函数中记录this和参数值的应用

---
+++ 编程题与分析题/debounce-demo.js
@@ -0,0 +1,44 @@
+function debounce(handler, delay){
+
+  delay = delay || 300;
+  var timer = null;
+
+  return function(){
+
+    var _self = this,
+        _args = arguments;
+
+    clearTimeout(timer);
+    timer = setTimeout(function(){
+      handler.apply(_self, _args);
+    }, delay);
+  }
+}
+
+// 不希望被频繁调用的函数
+function add(counterName) {
+  console.log(counterName + ":  " + this.index ++);
+}
+
+// 需要的上下文对象
+let counter = {
+  index: 0
+}
+
+// 防抖的自增函数，绑定上下文对象counter
+let db_add = debounce(add, 10).bind(counter)
+
+// 每隔500ms频繁调用3次自增函数，但因为防抖的存在，这3次内只调用一次
+setInterval(function() {
+  db_add("someCounter1");
+  db_add("someCounter2");
+  db_add("someCounter3");
+}, 500)
+
+
+/**
+ * 预期效果：
+ * 
+ * 每隔500ms，输出一个自增的数
+ * 即打印：0 1 2 3 ...
+ */
