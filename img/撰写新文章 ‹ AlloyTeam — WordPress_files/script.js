e8de63aeaac3cbd2860717e622b8fcbd5aee0a92
yaoyaohu <yaoyaohu@tencent.com>
2019-07-20 14:30:59 +0800

    add a png

--- img/撰写新文章 ‹ AlloyTeam — WordPress_files/script.js
+++
@@ -1,9 +0,0 @@
-jQuery( document ).ready( function () {
-
-	jQuery( '#resetpassform, #your-profile, #createuser' ).submit( function () {
-
-		jQuery( '#submit, #createusersub' ).append( '<input type="hidden" name="password_strength" id="password_strength" value="' + jQuery( '#pass-strength-result' ).attr( 'class' ) + '">' );
-
-	} );
-
-} );
